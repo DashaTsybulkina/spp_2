5b91aeb50d7bc1a27ac7ab1e2498b03477edc029
DashaTsybulkina <cybulkinadara@gmail.com>
2022-10-19 12:46:11 +0300

    added plugins

---
+++ FakerClass/generator/impl/DateTimeGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerClass.generator.impl
+{
+    public class DateTimeGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            Random random = new Random();
+            var year = random.Next(1, int.MaxValue);
+            var month = random.Next(1, 12);
+            var day = random.Next(1, DateTime.DaysInMonth(year, month));
+
+            return new DateTime(year, month, day);
+        }
+
+        public Type GetGenType()
+        {
+            return typeof(DateTime);
+        }
+    }
+}
---
+++ IntGenerator/IntGenerator.cs
@@ -0,0 +1,18 @@
+﻿using FakerClass.generator;
+
+namespace IntGenerator
+{
+    public class IntGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            Random random = new Random();
+            return random.Next(int.MinValue, int.MaxValue);
+        }
+
+        public Type GetGenType()
+        {
+            return typeof(int);
+        }
+    }
+}
